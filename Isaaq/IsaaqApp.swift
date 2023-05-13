d97931706a5ca95a0abacef0a5180e900e27dec1
alex <alex.shrestha88@gmail.com>
2023-05-13 09:36:31 +0200

    Initial Commit

---
+++ Isaaq/IsaaqApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsaaqApp.swift
+//  Isaaq
+//
+//  Created by Alex on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct IsaaqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
